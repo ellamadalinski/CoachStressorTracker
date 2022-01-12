becdcfecee12b5d9a96376d0d604fa549234cf2e
Ella Madalinski <ella.madalinski24@d155.org>
2022-01-12 08:10:17 -0600

    adding questions to survey, made survey class

---
+++ CoachStressorTracker/Survey.swift
@@ -0,0 +1,18 @@
+//
+//  Survey.swift
+//  CoachStressorTracker
+//
+//  Created by ELLA MADALINSKI on 1/12/22.
+//
+
+import Foundation
+class Survey{
+    var name: String
+    var date : Date
+    
+    init(n: String, d: Date){
+        name = n
+        date = d
+    }
+    
+}
